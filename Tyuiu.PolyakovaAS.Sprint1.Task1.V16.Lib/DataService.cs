f65ccd9cbb3e44394f6fae425252ee34c44629e0
KirillBurdov <Patrikandbooke@gmail.com>
2025-10-21 21:01:09 +0300

    Task2

--- Tyuiu.PolyakovaAS.Sprint1.Task1.V16.Lib/DataService.cs
+++ Tyuiu.PolyakovaAS.Sprint1.Task1.V16.Lib/DataService.cs
@@ -8,7 +8,7 @@ namespace Tyuiu.PolyakovaAS.Sprint1.Task1.V16.Lib
     {
         public double Calculate(double a, double x, double y)
         {
-            return (x * 5 * y + 2 * a);
+            return (x * y * 5 + 2 * a * x);
         }
     }
 }
