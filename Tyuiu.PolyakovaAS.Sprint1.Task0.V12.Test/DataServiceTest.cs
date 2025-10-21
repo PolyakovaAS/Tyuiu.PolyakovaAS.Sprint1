49c7090855d1ae48ad2d576bd8855c94b7be1a07
KirillBurdov <Patrikandbooke@gmail.com>
2025-10-21 20:34:47 +0300

    Add project files.

---
+++ Tyuiu.PolyakovaAS.Sprint1.Task0.V12.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿
+
+using Tyuiu.PolyakovaAS.Sprint1.Task0.V12.Lib;
+
+namespace Tyuiu.PolyakovaAS.Sprint1.Task0.V12.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(6, res);
+
+        }
+    }
+}
