f727dccf40fe1135958d5d3255d887bb70b7ece1
KirillBurdov <Patrikandbooke@gmail.com>
2025-10-21 20:57:49 +0300

    Task1

---
+++ Tyuiu.PolyakovaAS.Sprint1.Task1.V16.Lib/DataService.cs
@@ -0,0 +1,14 @@
+﻿
+
+using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.PolyakovaAS.Sprint1.Task1.V16.Lib
+{
+    public class DataService : ISprint1Task1V16
+    {
+        public double Calculate(double a, double x, double y)
+        {
+            return (x * 5 * y + 2 * a);
+        }
+    }
+}
---
+++ Tyuiu.PolyakovaAS.Sprint1.Task1.V16/Program.cs
@@ -0,0 +1,52 @@
+﻿
+
+using Tyuiu.PolyakovaAS.Sprint1.Task1.V16.Lib;
+
+namespace Tyuiu.PolyakovaAS.Sprint1.Task1.V16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Полякова А.С                                | ИИПБ-23-2";
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* Спринт #1                                                              *");
+            Console.WriteLine("* Тема:  Базовые навыки работы в C#                                      *");
+            Console.WriteLine("* Задание #1                                                             *");
+            Console.WriteLine("* Вариант #16                                                            *");
+            Console.WriteLine("* Выполнил: Полякова А. С                     | ИИПБ-23-2                *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                               *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные,*");
+            Console.WriteLine("* вычисляет результат по формуле x*5*y+2*a и печатает его на экране.     *");
+            Console.WriteLine("*                                                                        *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+
+            double a, x, y;
+
+            Console.WriteLine("Введите значение A:");
+            a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение X:");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение Y:");
+            y = Convert.ToDouble(Console.ReadLine());
+
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+
+
+
+            Console.WriteLine(ds.Calculate(a, x, y));
+
+            Console.ReadLine();
+        }
+    }
+}
