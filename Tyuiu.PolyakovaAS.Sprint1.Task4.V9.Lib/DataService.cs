f60bbc0eab276cee922b7d8526da351a9bb7660b
KirillBurdov <Patrikandbooke@gmail.com>
2025-11-08 15:31:02 +0300

    Task4

---
+++ Tyuiu.PolyakovaAS.Sprint1.Task4.V9.Lib/DataService.cs
@@ -0,0 +1,11 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.PolyakovaAS.Sprint1.Task4.V9.Lib
+{
+    public class DataService : ISprint1Task4V9
+    {
+        public double Calculate(double x, double y)
+        {
+            return Math.Round(Math.Log(x * y) / (x - (Math.Sqrt(1 + Math.Pow(y, 2)))), 3);
+        }
+    }
+}
---
+++ Tyuiu.PolyakovaAS.Sprint1.Task4.V9.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.PolyakovaAS.Sprint1.Task4.V9.Lib;
+namespace Tyuiu.PolyakovaAS.Sprint1.Task4.V9.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = 2.0;
+            double y = 1.0;
+            double wait = 1.183;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
